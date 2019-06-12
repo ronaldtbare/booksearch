8cef7d675df88aab01de81f67bd3bcc96c297a09
travisbare <travisbare@gmail.com>
2019-06-12 13:25:23 -0500

    added list component and css styling

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
         </div>
 
         <Search/>
+        <hr/>
         <Saved/>
 
       </div>
--- client/src/components/Saved.js
+++ client/src/components/Saved.js
@@ -1,16 +1,19 @@
-import React from "react";
-
+import React, {Component} from "react";
+import API from "../utils/API";
 import "../App.css";
-
+  
 function Saved() {
-    
-        return (
-            <div className="Search">
-                <h1>Saved</h1>
+
+    return (
+        <div className="list">
+            <h1>Book List</h1>
+            <div>
+                <p>Book Name</p>
 
             </div>
-        );
-}
 
+        </div>
+    );
+}
 
 export default Saved;
